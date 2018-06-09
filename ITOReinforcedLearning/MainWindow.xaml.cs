6e64ef15c88c6e796769b587cf7c659dfade879a
Kamila <kamila.korzec@gmail.com>
2018-06-09 14:32:18 +0200

    Dynamically set grid dimensions

--- ITOReinforcedLearning/MainWindow.xaml.cs
+++ ITOReinforcedLearning/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ITOReinforcedLearning.src;
 
 namespace ITOReinforcedLearning
 {
+    class UIGridTile
+    {
+        public bool Agent = false;
+        public bool Exit = false;
+        public List<PossibleDirections> Walls = new List<PossibleDirections> { };
+
+        public UIGridTile() { }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -37,7 +47,30 @@ namespace ITOReinforcedLearning
 
         private void UpdateGrid(object sender, RoutedEventArgs e)
         {
+            ObservableCollection<ObservableCollection<UIGridTile>> list = new ObservableCollection<ObservableCollection<UIGridTile>>{ };
+            this.LearningGrid.Columns.Clear();
 
+            int dimension = int.Parse(this.Dimension.Text);
+
+            for(int i = 0; i < dimension; i++)
+            {
+                var row = new ObservableCollection<UIGridTile> { };
+                for(int j = 0; j < dimension; j++)
+                {
+                    row.Add(new UIGridTile());
+                }
+
+                list.Add(new ObservableCollection<UIGridTile>(row));
+                var column = new DataGridTextColumn
+                {
+                    Width = new DataGridLength(1, DataGridLengthUnitType.Star)
+                };
+                this.LearningGrid.Columns.Add(column);
+            }
+
+            double margin = 8;
+            this.LearningGrid.RowHeight = (this.LearningGrid.RenderSize.Height - margin) / dimension;
+            this.LearningGrid.ItemsSource = list;
         }
     }
 }
